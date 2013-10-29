58e8e3391ea4f496c19d87af51439d1ac736ee83
Mercieca <mpm123@tsc-487.techsmith.com>
2013-10-29 09:21:54 -0400

    Adding base graident

--- WebContent/run.js
+++ WebContent/run.js
@@ -1,2 +1,21 @@
+var div;
+
 function run() {
+	var body = document.getElementById("body");
+	
+	div = document.createElement("div");
+	
+	div.className = "blue box";
+	
+	div.style.width = "500px";
+	div.style.height = "200px";
+	div.style.top = "100px";
+	div.style.left = "100px";
+	body.appendChild(div);
+	
+	setTimeout("doStuff()", 100);
+}
+
+function doStuff() {
+	div.style.cssText = div.style.cssText + ";-webkit-transform: rotateY(360deg)";
 }
